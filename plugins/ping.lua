16a1af1ec2ace6f649a612db83f1b904dff617a3
MeGaReborn <rezabayat594@gmail.com>
2017-03-14 23:06:32 +0330

    Create ping.lua

---
+++ plugins/ping.lua
@@ -0,0 +1,16 @@
+--start by @Tele_Sudo
+local datebase = {
+   "PONG...",
+  }
+local function run(msg, matches) 
+return datebase[math.random(#datebase)]
+end
+return {
+  patterns = {
+    "^[/#+×!$]([Pp][Ii][Nn][Gg])",
+  },
+  run = run
+}
+
+--end by @Tele_Sudo
+--Channel @LuaError
