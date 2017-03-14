fb1d1967a88a202a6c0f02e6298a543752589ad4
MeGaReborn <rezabayat594@gmail.com>
2017-03-14 23:21:49 +0330

    Update autolaunch.sh

--- autolaunch.sh
+++ autolaunch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 COUNTER=1
 while(true) do
-./beyond.sh
+./megareborn.sh
 curl "https://api.telegram.org/bot279192153:AAE6V2Stk0lEZPdV1kK5NmQMzPQcDHnLc1s/sendmessage" -F "chat_id=308444837" -F "text=#NEWCRASH-#TeleBeyond-Reloaded-${COUNTER}-times"
 let COUNTER=COUNTER+1 
 done
