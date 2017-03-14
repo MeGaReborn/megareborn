f10c3138cdaf656f6fe90a783aec5649731b5cc9
MeGaReborn <rezabayat594@gmail.com>
2017-03-14 23:10:49 +0330

    Create time.lua

---
+++ plugins/time.lua
@@ -0,0 +1,16 @@
+--Start By @Tele_Sudo
+
+function run(msg, matches)
+local url , res = http.request('http://api.gpmod.ir/time/')
+if res ~= 200 then return "No connection" end
+local jdat = json:decode(url)
+local text = '*Ir Time:* _'..jdat.FAtime..'_\n*Ir Data:* _'..jdat.FAdate..'_\n------------\n*En Time:* _'..jdat.ENtime..'_\n *En Data:* _'..jdat.ENdate.. '_\n'
+  tdcli.sendMessage(msg.chat_id_, 0, 1, text, 1, 'md')
+end
+return {
+  patterns = {"^[/!]([Tt][iI][Mm][Ee])$"}, 
+run = run 
+}
+--End By @Tele_Sudo
+--Channel @LuaError
+
