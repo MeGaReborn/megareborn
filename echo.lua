475d9319e5c8d8c532f60f2073b766dc4ed77cf2
MeGaReborn <rezabayat594@gmail.com>
2017-03-14 22:53:23 +0330

    Create echo.lua

---
+++ echo.lua
@@ -0,0 +1,10 @@
+local function run(msg, matches)
+if matches[1] == 'echo' then
+local pext = matches[2]
+tdcli.sendMessage(msg.to.id, 0,1, pext,1,'html')
+end
+end
+
+return{ patterns= {'^[/!](echo) (.*)$'}, run=run}
+
+-- end 
